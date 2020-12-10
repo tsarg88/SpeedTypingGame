6aeea582168a467f5f5b2e69cfd1b5f2e6273f0e
umbur <work1415@gmail.com>
2020-12-09 22:27:25 -0700

    Sets up app.js and styling

---
+++ speed-typing-game/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>How fast can You type?</h1>
+      <textarea />
+      <h4>Time Remaining:</h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
+    </>
+  );
+}
+
+export default App;
